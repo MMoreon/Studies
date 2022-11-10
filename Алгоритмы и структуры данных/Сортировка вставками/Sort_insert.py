28d2e06d7aa2fe3b9b8c1a98cdf842f6c9a3194b
MMoreon <96682195+MMoreon@users.noreply.github.com>
2022-11-10 12:48:48 +0300

    Add files via upload

---
+++ Алгоритмы и структуры данных/Сортировка вставками/Sort_insert.py
@@ -0,0 +1,25 @@
+import random as r 
+import time
+
+def sort(lst):
+    for i in range(1, len(lst)):
+        key = lst[i]
+        j = i-1
+        while j >=0 and key < lst[j] :
+            lst[j+1] = lst[j]
+            j -= 1
+        lst[j+1] = key
+
+t_start = time.monotonic()
+
+count = 10000
+lst = [r.randint(0, count) for i in range(count)]
+
+print("Random" + str(lst))
+sort(lst)
+print("Sort: " + str(lst))
+
+t_stop = time.monotonic()
+
+t_run = t_stop - t_start
+print(t_run)
