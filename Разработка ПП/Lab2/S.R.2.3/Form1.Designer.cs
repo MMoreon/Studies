20e6a4d2b494f1d5dce2d4d57dc08ca2418bbbbb
MMoreon <96682195+MMoreon@users.noreply.github.com>
2022-12-07 20:26:03 +0500

    Add files via upload

---
+++ Разработка ПП/Lab2/S.R.2.3/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace S.R._2._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 80);
+            this.label1.MaximumSize = new System.Drawing.Size(121, 121);
+            this.label1.MinimumSize = new System.Drawing.Size(121, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 121);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 319);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Разработка ПП/Lab2/S.R.2.3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S.R._2._3
+{
+    public partial class Form1 : Form
+    {
+        int msec = 0, count = 10;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (msec == 99)
+            {
+                count--;
+                label1.Text = count.ToString();
+                msec = 0;
+            }
+            else msec++;
+
+            if (count == 0)
+            {
+                timer1.Enabled = false;
+            }
+
+            if (timer1.Enabled != true)
+            {
+                MessageBox.Show("ПУСК", "Внимание!");
+                this.Close();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = Convert.ToString(count);
+            timer1.Interval = 10;
+            timer1.Enabled = true;
+        }
+    }
+}
