c905e5a190699644cc32cd8ce42cc0b9a514008e
MMoreon <96682195+MMoreon@users.noreply.github.com>
2022-12-07 21:28:36 +0500

    Add files via upload

---
+++ Разработка ПП/Lab4/L.R.4.3/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace L.R._4._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Нахождение максимума и минимума";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Заполнить таблицу случайными числами";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(208, 195);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(226, 94);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(113, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Найти максимум";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(226, 160);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(107, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Найти минимум";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 326);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Разработка ПП/Lab4/L.R.4.3/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L.R._4._3
+{
+    public partial class Form1 : Form
+    {
+        
+            const int n = 5; // n- число столбцов
+            const int m = 5; // m- число строк
+            int i, j;
+            /* в k и l будем заносить номера ячеек с max/min значениями */
+            int k, l;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Цвет фона таблицы задаем такой же, как у формы:
+            dataGridView1.BackgroundColor =
+            this.BackColor;
+            // Убираем внешнюю границу компонента:
+            dataGridView1.BorderStyle = BorderStyle.None;
+            //задаем число столбцов
+            dataGridView1.ColumnCount = n;
+            //задаем число строк
+            dataGridView1.RowCount = m;
+            // убираем шапку таблицы:
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+            // Убираем полосу прокрутки:
+            dataGridView1.ScrollBars = ScrollBars.None;
+            // Задаем размеры таблицы (при необходимости измените):
+            dataGridView1.Width = 320;
+            dataGridView1.Height = 160;
+            // Задаем параметры шрифта для ячеек таблицы:
+            dataGridView1.Font = new Font("Arial", 14);
+        }
+
+            private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы действительно хотите выйти ? ", "Выход", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Задаем белый цвет всем ячейкам:
+            dataGridView1.DefaultCellStyle.BackColor = Color.White;
+            // «Выключаем» checkbox–ы :
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            // Заполняем ячейки:
+            Random r = new Random();
+            for (i = 0; i < n; i++)
+                for (j = 0; j < m; j++)
+                    dataGridView1.Rows[i].Cells[j].Value = (r.Next(200) - 100).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged_1(object sender, EventArgs e)
+        {
+            int min = Convert.ToInt16(dataGridView1.Rows[0].Cells[0].Value);
+            for (i = 0; i < n; i++)
+                for (j = 0; j < m; j++)
+                    if (Convert.ToInt16(dataGridView1.Rows[i].
+                    Cells[j].Value) < min)
+                    {
+                        min = Convert.ToInt16(dataGridView1.Rows[i].Cells[j].Value);
+                        k = i;
+                        l = j;
+                    }
+            if (checkBox2.Checked)
+                dataGridView1.Rows[k].Cells[l].Style.BackColor = Color.LightBlue;
+            else
+                dataGridView1.Rows[k].Cells[l].Style.BackColor = Color.White;
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            // За max принимаем значение ячейки (0,0):
+            int max = Convert.ToInt16(dataGridView1.Rows[0].Cells[0].Value);
+            //далее- по стандартному алгоритму поиска максимума:
+            for (i = 0; i < n; i++)
+                for (j = 0; j < m; j++)
+                    if (Convert.ToInt16(dataGridView1.Rows[i].
+                    Cells[j].Value) > max)
+                    {
+                        max = Convert.ToInt16(dataGridView1.Rows[i].Cells[j].Value);
+                        k = i;
+                        l = j;
+                    }
+            // Если кнопка «Включена» -орашиваем найденную ячейку в розовый цвет:
+            if (checkBox1.Checked)
+                dataGridView1.Rows[k].Cells[l].Style.BackColor = Color.LightPink;
+            // Если нет- меняем цвет ячейки на фоновй белый:
+            else
+                dataGridView1.Rows[k].Cells[l].Style.BackColor = Color.White;
+        }
+    }
+}
