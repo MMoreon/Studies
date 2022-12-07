24fdfc247e02f9be5621fc896958b741fd9e2749
MMoreon <96682195+MMoreon@users.noreply.github.com>
2022-12-07 20:15:22 +0500

    Add files via upload

---
+++ Разработка ПП/Lab3/S.R.3.2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S.R._3._2
+{
+    public partial class Form1 : Form
+    {
+        Form2 f2 = new Form2();
+        double sum = 0;
+        string name, res;
+        double cost;
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            f2.ShowDialog();
+            name = f2.name;
+            cost = f2.cost;
+            res += name + " " + Convert.ToString(cost) + "\n";
+            label1.Text = res;
+            sum += cost;
+            label2.Text = "Итого: " + Convert.ToString(sum) + Convert.ToString("р.");
+        }
+    }
+}
---
+++ Разработка ПП/Lab3/S.R.3.2/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S.R._3._2
+{
+    public partial class Form2 : Form
+    {
+        public string name;
+        public double cost;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.name = Convert.ToString(textBox1.Text);
+                this.cost = Convert.ToDouble(textBox2.Text);
+                textBox1.Clear();
+                textBox2.Clear();
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте корректность входных данных!", "Внимание!");
+            }
+        }
+    }
+}
