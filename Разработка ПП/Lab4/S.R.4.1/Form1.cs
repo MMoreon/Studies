8405c0dea55968baa2c7fc39423a89eb372c6c20
MMoreon <96682195+MMoreon@users.noreply.github.com>
2022-12-08 16:15:56 +0500

    Add files via upload

---
+++ Разработка ПП/Lab4/S.R.4.1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace S.R._4._1
+{
+    public partial class Form1 : Form
+    {
+        int[] Mas;
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label3.Text = "";
+            Mas = new int[10];
+            Random n = new Random();
+            for (i = 0; i < 10; i++)
+                Mas[i] = n.Next(-100, 100);
+            // Выводим на экран:
+            foreach (int elem in Mas)
+                label1.Text = label1.Text + elem + " ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < Mas.Length; i++)
+            {
+                if (Mas[i] == Convert.ToInt32(textBox1.Text))
+                {
+                    label3.Text = "Элемент " + textBox1.Text + " на " + Convert.ToString(i + 1) + " позиции.";
+                    break;
+                }
+                else
+                {
+                    label3.Text = "Такого элемента нет";
+                }
+            }
+            textBox1.Clear();
+        }
+    }
+}
