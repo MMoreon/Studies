d10477da0d79aaaad7dcf01ecbd8099100c7572d
MMoreon <96682195+MMoreon@users.noreply.github.com>
2022-12-08 17:09:54 +0500

    Add files via upload

---
+++ Разработка ПП/Lab4/S.R.4.3/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace L.R._4._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Обработать данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Успеваемость студентов по Лбъективно ориентированному программированию";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(651, 257);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ФИО";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Группа";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Лабораторная 1";
+            this.Column3.Name = "Column3";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Лабораторная 2";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Лабораторная 3";
+            this.Column7.Name = "Column7";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(383, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(383, 340);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "1";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 365);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Разработка ПП/Lab4/S.R.4.3/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L.R._4._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /* Создаем элементы списка comboBox для второй
+            колонки:(не забудьте, индексация строк и столбцов
+            начинается с нуля) */
+            ((DataGridViewComboBoxColumn)dataGridView1.Columns[1]
+            ).Items.AddRange(new string[] { "ПИНб-21а", "ПИНб-21б" });
+            // Задаем цвет фона компонента:
+            dataGridView1.BackgroundColor = this.BackColor;
+            // Убираем рамку:
+            dataGridView1.BorderStyle = BorderStyle.None;
+            // Задаем размеры таблицы:
+            dataGridView1.Width = 700;
+            dataGridView1.Height = 160;
+            // Создаем вертикальную полосу прокрутки:
+            dataGridView1.ScrollBars = ScrollBars.Vertical;
+            // Задаем выравнивание текста в ячейках по центру:
+            dataGridView1.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i, countOne = 0 , countTwo = 0;
+            // Если у нас 5 начальных колонок, формируем еще одну
+            if (dataGridView1.ColumnCount == 5)
+                dataGridView1.Columns.Add("Column5","Допуск к экзамену");
+            dataGridView1.Columns[5].Width = 140;
+            for (i = 0; i < dataGridView1.RowCount; i++)
+                /* Проверка по флагу*/
+                if (Convert.ToBoolean(dataGridView1.Rows[i].Cells[2].Value) == true
+                    && Convert.ToBoolean(dataGridView1.Rows[i].Cells[3].Value) == true
+                    && Convert.ToBoolean(dataGridView1.Rows[i].Cells[4].Value) == true)
+                { 
+                    // Если все сдано- пишем «Допущен» и
+                    // закрашиваем строку:
+                    dataGridView1.Rows[i].Cells[5].Value = "Допущен";
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.MistyRose;
+                    if (Convert.ToString(dataGridView1.Rows[i].Cells[1].Value) == "ПИНб-21а")
+                    {
+                        countOne += 1;
+                    }
+                    if (Convert.ToString(dataGridView1.Rows[i].Cells[1].Value) == "ПИНб-21б")
+                    {
+                        countTwo += 1;
+                    }
+                }
+                else
+                {
+                    dataGridView1.Rows[i].Cells[5].Value = "";
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.White;
+                }
+            label2.Text = "В группе ПИНб-21а допущено: " + Convert.ToString(countOne);
+            label3.Text = "В группе ПИНб-21б допущено: " + Convert.ToString(countTwo);
+
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+    
+
