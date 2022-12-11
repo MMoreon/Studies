bde1aba11feaf7cfc6a693cb7d8920d6dee27444
MMoreon <96682195+MMoreon@users.noreply.github.com>
2022-12-11 18:24:23 +0500

    Add files via upload

---
+++ Разработка ПП/Lab5/S.R.5.1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace S.R._5._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("sbyte");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("byte");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("short");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("ushort");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("int");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("uint");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("long");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("ulong");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Integral", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6,
+            treeNode7,
+            treeNode8});
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("char");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Characters", new System.Windows.Forms.TreeNode[] {
+            treeNode10});
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("float");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("double");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Floating", new System.Windows.Forms.TreeNode[] {
+            treeNode12,
+            treeNode13});
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("decimal");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Decimal", new System.Windows.Forms.TreeNode[] {
+            treeNode15});
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("bool");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Boolean", new System.Windows.Forms.TreeNode[] {
+            treeNode17});
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Simple types", new System.Windows.Forms.TreeNode[] {
+            treeNode9,
+            treeNode11,
+            treeNode14,
+            treeNode16,
+            treeNode18});
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Enum types");
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Struct types");
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Nullable types");
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Value types", new System.Windows.Forms.TreeNode[] {
+            treeNode19,
+            treeNode20,
+            treeNode21,
+            treeNode22});
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("object");
+            System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("string");
+            System.Windows.Forms.TreeNode treeNode26 = new System.Windows.Forms.TreeNode("class");
+            System.Windows.Forms.TreeNode treeNode27 = new System.Windows.Forms.TreeNode("Class types", new System.Windows.Forms.TreeNode[] {
+            treeNode24,
+            treeNode25,
+            treeNode26});
+            System.Windows.Forms.TreeNode treeNode28 = new System.Windows.Forms.TreeNode("Interface types");
+            System.Windows.Forms.TreeNode treeNode29 = new System.Windows.Forms.TreeNode("Array types");
+            System.Windows.Forms.TreeNode treeNode30 = new System.Windows.Forms.TreeNode("Delegate types");
+            System.Windows.Forms.TreeNode treeNode31 = new System.Windows.Forms.TreeNode("Reference types", new System.Windows.Forms.TreeNode[] {
+            treeNode27,
+            treeNode28,
+            treeNode29,
+            treeNode30});
+            System.Windows.Forms.TreeNode treeNode32 = new System.Windows.Forms.TreeNode("C# Data types", new System.Windows.Forms.TreeNode[] {
+            treeNode23,
+            treeNode31});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 13);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Узел12";
+            treeNode1.Text = "sbyte";
+            treeNode2.Name = "Узел13";
+            treeNode2.Text = "byte";
+            treeNode3.Name = "Узел14";
+            treeNode3.Text = "short";
+            treeNode4.Name = "Узел15";
+            treeNode4.Text = "ushort";
+            treeNode5.Name = "Узел16";
+            treeNode5.Text = "int";
+            treeNode6.Name = "Узел17";
+            treeNode6.Text = "uint";
+            treeNode7.Name = "Узел18";
+            treeNode7.Text = "long";
+            treeNode8.Name = "Узел19";
+            treeNode8.Text = "ulong";
+            treeNode9.Name = "Узел7";
+            treeNode9.Text = "Integral";
+            treeNode10.Name = "Узел20";
+            treeNode10.Text = "char";
+            treeNode11.Name = "Узел8";
+            treeNode11.Text = "Characters";
+            treeNode12.Name = "Узел21";
+            treeNode12.Text = "float";
+            treeNode13.Name = "Узел22";
+            treeNode13.Text = "double";
+            treeNode14.Name = "Узел9";
+            treeNode14.Text = "Floating";
+            treeNode15.Name = "Узел23";
+            treeNode15.Text = "decimal";
+            treeNode16.Name = "Узел10";
+            treeNode16.Text = "Decimal";
+            treeNode17.Name = "Узел24";
+            treeNode17.Text = "bool";
+            treeNode18.Name = "Узел11";
+            treeNode18.Text = "Boolean";
+            treeNode19.Name = "Узел3";
+            treeNode19.Text = "Simple types";
+            treeNode20.Name = "Узел4";
+            treeNode20.Text = "Enum types";
+            treeNode21.Name = "Узел5";
+            treeNode21.Text = "Struct types";
+            treeNode22.Name = "Узел6";
+            treeNode22.Text = "Nullable types";
+            treeNode23.Name = "Узел1";
+            treeNode23.Text = "Value types";
+            treeNode24.Name = "Узел29";
+            treeNode24.Text = "object";
+            treeNode25.Name = "Узел30";
+            treeNode25.Text = "string";
+            treeNode26.Name = "Узел31";
+            treeNode26.Text = "class";
+            treeNode27.Name = "Узел25";
+            treeNode27.Text = "Class types";
+            treeNode28.Name = "Узел26";
+            treeNode28.Text = "Interface types";
+            treeNode29.Name = "Узел27";
+            treeNode29.Text = "Array types";
+            treeNode30.Name = "Узел28";
+            treeNode30.Text = "Delegate types";
+            treeNode31.Name = "Узел2";
+            treeNode31.Text = "Reference types";
+            treeNode32.Name = "Узел0";
+            treeNode32.Text = "C# Data types";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode32});
+            this.treeView1.Size = new System.Drawing.Size(375, 293);
+            this.treeView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 318);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Иерархия типов данных C#";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
