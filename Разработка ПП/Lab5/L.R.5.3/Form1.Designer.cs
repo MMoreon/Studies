c7236d69946511d41c1c71d4e6ae52799d4cde44
MMoreon <96682195+MMoreon@users.noreply.github.com>
2022-12-11 17:56:08 +0500

    Add files via upload

---
+++ Разработка ПП/Lab5/L.R.5.3/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace L.R._5._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите дату первого лабораторного занятия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = ": Введите время начала занятия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(342, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(315, 52);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(228, 91);
+            this.maskedTextBox1.Mask = "00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(339, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Сгенерировать расписание";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(440, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 395);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Разработка ПП/Lab5/L.R.5.3/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L.R._5._3
+{
+    public partial class Form1 : Form
+    {
+        DateTime d0, d1, d2;
+        int t = 100; // Степень прозрачности формы
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+            timer1.Interval = 500;
+            timer1.Interval = 50;
+            label1.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+        }
+        //выход
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Запускаем генерацию событий Tick для второго таймера
+            timer2.Enabled = true;
+        }
+        // С каждым тиком делаем форму все более прозрачной:
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (t != 0)
+            {
+                this.Opacity = (float)t / 100;
+                this.Refresh();//отрисовываем форму заново
+                t--;
+            }
+            else this.Close();
+        }
+
+        /*С заданным периодом обновляем текущую дату, время, день недели
+   (особо актуально для секунд) */
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // В d0 записываем текущую дату и время:
+            d0 = DateTime.Now;
+            label1.Text = "Today is" + Convert.ToString(d0.DayOfWeek) + " " + Convert.ToString(d0);
+        }
+        //генерация расписания
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label4.Text = "";
+                //В d1 записываем выбранную дату из DateTimePicker:
+                d1 = dateTimePicker1.Value;
+                // Допустим, предполагается 8 лабораторных занятий:
+                for (int i = 1; i < 9; i++)
+                {
+                    label4.Text = label4.Text + d1.ToLongDateString() + "\n";
+                    // Сдвигаем d1 на 2 недели вперед:
+                    d1 = d1.AddDays(14);
+                }
+                //В d2 записываем время из maskedTextBox :
+                d2 = Convert.ToDateTime(maskedTextBox1.Text);
+                // Допустим, занимаемся 2 пары с перерывом в 10 минут:
+                label5.Text = "Время занятий: " + maskedTextBox1.Text + "-" + d2.AddHours(3).AddMinutes(10).ToShortTimeString();
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте корректность входных данных!");
+            }
+        }
+    }
+        /*Здесь функция DayOfWeek возвращает день недели. Now –
+        возвращает текущие дату и время; ToLongDateString() – дата с названием
+        месяца; AddDays(n) – сдвигает дату на n дней, AddMinutes(n)-
+        прибавляет (если n положительное) n минут, AddHours(n)- прибавляет n
+        часов.
+         Можно использовать форматный вывод дат и времени. Например,
+        вместо строки вывода текущих дат и времени:
+        label1.Text="Today is
+        "+Convert.ToString(d0.DayOfWeek)
+        + " "+ Convert.ToString(d0);
+        попробуйте написать:
+        label1.Text = "Сегодня "+DateTime.Now.ToString("d
+        MMMM yyyy, dddd HH:mm:ss"); (приятный плюс- день недели
+        выведется в русском варианте).
+         Подробнее о спецификаторах формата см. в приложении.
+         Свойство Opacity (степень прозрачности) принимает значения от 0
+         до 100% (в программе 1% - 0,01; 99% - 0,99)  */
+}
