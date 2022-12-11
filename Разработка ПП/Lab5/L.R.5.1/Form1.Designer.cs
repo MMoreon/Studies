96ca18558588b9f9b81df97e81c318a2fbc31d08
MMoreon <96682195+MMoreon@users.noreply.github.com>
2022-12-11 16:35:57 +0500

    Add files via upload

---
+++ Разработка ПП/Lab5/L.R.5.1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace L.R._5._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Угадайте задуманное число от 1 до 999";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 194);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(298, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 236);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(322, 24);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(122, 19);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 19);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 260);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Угадай число";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Разработка ПП/Lab5/L.R.5.1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L.R._5._1
+{
+    public partial class Form1 : Form
+    {
+        
+        const int t = 60;
+        int zadumano = 0;
+        int ostalos = 60;
+        int nomer_popitki = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Focus();
+            label2.Text = "";
+            toolStripStatusLabel1.Text = "У вас осталось " +
+            Convert.ToString(t) + " сек";
+        toolStripStatusLabel2.Text = " Попыток: 0 ";
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            // (при необходимости перенастроите)
+            progressBar1.Maximum = t;
+            progressBar1.Value = t;
+            progressBar1.Step = 1;
+            Random n = new Random();
+            zadumano = n.Next(1000); // от 1 до 999
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ostalos--;
+            progressBar1.Value--;
+            toolStripStatusLabel1.Text = "У вас осталось" +
+           Convert.ToString(ostalos) + " сек ";
+            if (ostalos == 0)
+            {
+                timer1.Enabled = false;
+                textBox1.Enabled = false;
+                progressBar1.Enabled = false;
+                label2.Text = "Увы, время истекло...";
+            }
+        }
+        private void textBox1_KeyPress(object sender,KeyPressEventArgs e)
+        {
+            // Если в поле нажат Enter
+            if (e.KeyChar.Equals((char)13))
+            {
+                try
+                {
+                    if (Convert.ToInt16(textBox1.Text) == zadumano)
+                    {
+                        timer1.Enabled = false;
+                        textBox1.Enabled = false;
+                        label2.Text = "Вы угадали!,задумывалось число " + Convert.ToString(zadumano);
+                    };
+                    if (Convert.ToInt16(textBox1.Text) > zadumano)
+                        label2.Text = "Задуманное число меньше";
+                    if (Convert.ToInt16(textBox1.Text) < zadumano)
+                        label2.Text = "Задуманное число больше";
+                } // для try
+                  catch
+                {
+                    label2.Text = "Некорректные входные данные!";
+                }
+                    nomer_popitki++;
+                    toolStripStatusLabel2.Text = " Попыток:" +
+                   Convert.ToString(nomer_popitki);
+                    textBox1.Text = "";
+                    textBox1.Focus();
+                } // для if
+            } // закрыли функцию textBox1_KeyPress
+        }
+    }
+
